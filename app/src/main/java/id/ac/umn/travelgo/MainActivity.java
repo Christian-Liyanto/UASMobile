ad4211e4e74442cd4fe1cfa663742c2538d3557a
Christian-Liyanto <christian.liyanto@student.umn.ac.id>
2021-12-06 22:58:43 +0700

    Fixed Orientation & Connection Check

--- app/src/main/java/id/ac/umn/travelgo/MainActivity.java
+++ app/src/main/java/id/ac/umn/travelgo/MainActivity.java
@@ -1,13 +1,17 @@
 package id.ac.umn.travelgo;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
         signin = (Button) findViewById(R.id.signin);
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
+
+        if(!isConnected(MainActivity.this)) {
+            showCustomDialog();
+        }
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,5 +91,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private boolean isConnected(MainActivity mainActivity) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) mainActivity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+
+        if((activeNetwork != null && activeNetwork.getType() == connectivityManager.TYPE_WIFI) || (activeNetwork != null && activeNetwork.getType() == connectivityManager.TYPE_MOBILE)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private void showCustomDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Please connect to the internet to proceed further")
+                .setCancelable(false)
+                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        System.exit(0);
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
 }
 // Script By Christian Liyanto - 00000033739
+// Script By Kevin Hindra Jaya - 00000033631
